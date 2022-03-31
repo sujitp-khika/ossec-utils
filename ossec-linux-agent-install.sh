cc96b6ab9f3cbf37880dd2a143a33570bf981d4e
Khika Technologies Private Limited <101575053+khikatech@users.noreply.github.com>
2022-04-01 03:05:26 +0530

    Agent Install Automation Scripts

---
+++ ossec-linux-agent-install.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+# Copyright 2022 KHIKA Technologies Private Limited. All rights reserved.
+# This script is written for automation of ossec-agent installation for CentOS & Ubuntu
+# Official ossec site link for downloading source code and other documentation related information is available through the link https://www.ossec.net/download-ossec/
+main()
+{
+os_name=`grep "PRETTY_NAME" /etc/os-release |  cut -f2 -d"="`
+exec >ossec_installation.log
+export NON_INT=1
+export INPUTTEXT="yes"
+user=`whoami`
+
+if [ "$user" != "root" ]; then
+   echo "You must be root user"
+   exit
+fi
+
+if [[ "$os_name" == *"CentOS Linux"* ]] || [[ "$os_name" == *"Ubuntu"* ]]; then
+        echo "OS Name="$os_name
+	echo "############################################################################"
+        if [[ "$os_name" == *"CentOS Linux"* ]]; then
+		echo "wget installation started"
+                sudo yum install wget -y
+		echo "wget installation completed"
+		echo "############################################################################"
+		echo "ossec-agent packages download started"
+                wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo -E bash
+		echo " ossec-agent packages download completed"
+		echo "############################################################################"
+		echo "ossec-agent installation started"
+                sudo yum install ossec-hids-agent -y
+		echo "ossec-agent installation complted"
+		echo "############################################################################"
+                sed -E -i "s/[0-9]{1,3}(\.[0-9]{1,3}){3}/$server_ip/g" /var/ossec/etc/ossec.conf
+		echo "server IP updated:" $server_ip
+                touch /var/ossec/queue/rids/sender
+		yes | sudo /var/ossec/bin/manage_agent -i "$device_key"
+		echo "Device Added with key:" $device_key
+        elif [[ "$os_name" == *"Ubuntu"* ]]; then
+		echo "wget installation started"
+                sudo apt-get install wget -y
+                echo "wget installation completed"
+		echo "############################################################################"
+		echo "ossec-agent packages download started"
+		wget -q -O - https://updates.atomicorp.com/installers/atomic | sudo -E bash
+                echo "ossec-agent packages download completed"
+		echo "############################################################################"
+		echo "Ubuntu update started"
+		sudo apt-get update
+		echo "Ubuntu update completed"
+		echo "############################################################################"
+                echo "ossec-agent installation started"
+		sudo apt-get install ossec-hids-agent -y
+		echo "ossec-agent installation complted"
+		echo "############################################################################"
+                sed -E -i "s/[0-9]{1,3}(\.[0-9]{1,3}){3}/$server_ip/g" /var/ossec/etc/ossec.conf
+		echo "server IP updated:" $server_ip
+                touch /var/ossec/queue/rids/sender
+		yes | sudo /var/ossec/bin/manage_agents -i "$device_key"
+		echo "Device Added with key:" $device_key
+        fi
+                sed -i 's/logcollector.remote_commands=0/logcollector.remote_commands=1/g' /var/ossec/etc/internal_options.conf
+                sed -i 's/remoted.verify_msg_id=1/remoted.verify_msg_id=0/g' /var/ossec/etc/internal_options.conf
+                sudo /var/ossec/bin/ossec-control start
+                sudo /var/ossec/bin/ossec-control restart
+else
+        echo "OS Name="$os_name
+        echo "If OS Name is other than CENTOS/UBUNTU Please follow the manual installation step guide to download and install from https://www.ossec.net/download-ossec/"
+        echo "For HELP please contact us on https://khika.com/"
+        exit
+fi
+}
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 --server_ip 'VALUE'  --device_key 'KEY'"
+   echo -e "\t--server_ip server ip"
+   echo -e "\t--device_key device key"
+   exit 1 # Exit script after printing help
+}
+while getopts ":-:" opt
+do
+   case "$OPTARG" in
+      server_ip ) server_ip="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 )) ;;
+      device_key ) device_key="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 )) ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
+# Print helpFunction in case parameters are empty
+if [ -z "$server_ip" ] || [ -z "$device_key" ]
+then
+	echo "Some or all of the parameters are empty";
+	helpFunction
+fi
+main
+
---
+++ ossec-win-agent-install.ps1
@@ -0,0 +1,71 @@
+﻿# Copyright 2022 KHIKA Technologies Private Limited. All rights reserved.
+# This script is written for automation of ossec-agent installation for Windows
+# Official ossec site link for downloading source code and other documentation related information is available through the link https://www.ossec.net/download-ossec/
+
+param (
+    [Parameter(Mandatory=$true)][string]$server_ip,$device_key
+ )
+
+$Logfile = "$PSScriptRoot\ossec-installation.log"
+function WriteLog{
+    Param ([string]$LogString)
+    $Stamp = (Get-Date).toString("yyyy/MM/dd HH:mm:ss")
+    $LogMessage = "$Stamp $LogString"
+    Add-content $LogFile -value $LogMessage
+ }
+
+
+$elevated = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+if ($elevated -ne 'True'){
+     Write-output "You are not Administrator User. You need to Run PowerShell Script in as 'Windows PowerShell ISE with Administrator' to install the ossec-agent"
+    break;
+    }else{
+    
+        WriteLog "ossec-agent-win32-3.7.0-24343.exe downloading started"
+        $url = "https://updates.atomicorp.com/channels/atomic/windows/ossec-agent-win32-3.7.0-24343.exe"
+        $outpath = "$PSScriptRoot/ossec-agent-win32-3.7.0-24343.exe"
+        $config = 'C:\Program Files (x86)\ossec-agent\ossec.conf'
+        $internalConfig = 'C:\Program Files (x86)\ossec-agent\internal_options.conf'
+        $srvName = "OssecSvc"
+        $wc = New-Object System.Net.WebClient
+        $wc.DownloadFile($url, $outpath)
+        WriteLog "ossec-agent-win32-3.7.0-24343.exe downloading completed"
+
+        WriteLog "ossec-agent installation started"
+        Start-Process -Wait -FilePath "$PSScriptRoot/ossec-agent-win32-3.7.0-24343.exe" -ArgumentList "/S" -PassThru
+        WriteLog "ossec-agent installation completed"
+
+        (Get-Content -path $internalConfig -Raw) -replace 'logcollector.remote_commands=0','logcollector.remote_commands=1' | Set-Content -Path $internalConfig
+        (Get-Content -path $internalConfig -Raw) -replace 'remoted.verify_msg_id=1','remoted.verify_msg_id=0' | Set-Content -Path $internalConfig
+        New-Item -Path 'C:\Program Files (x86)\ossec-agent\rids\sender'
+        Add-Content $config "`n<ossec_config> <client> <server-ip>$server_ip</server-ip> </client> </ossec_config>"
+        Start-Sleep -s 10
+
+        
+        # Adding device
+        WriteLog "Adding Device, Server_IP:$server_ip device_key:$device_key"
+        Write-Output "`r`nAdding Device"
+        echo "y" | & "c:\Program Files (x86)\ossec-agent\manage_agents.exe" "-i $device_key" "y`r`n"
+
+        Start-Sleep -s 10
+        WriteLog "Device Added"
+
+        WriteLog "Starting service."
+        Write-Output "Starting service."
+        Start-Service $srvName
+        $srvStat = Get-Service $srvName
+        Write-Output "$($srvName) is now $($srvStat.status)"
+        WriteLog "$($srvName) is now $($srvStat.status)"
+
+        Start-Sleep -s 15
+
+        WriteLog "Restarting service."
+        Write-Output "Restarting service."
+        Restart-Service $srvName
+        $srvStat = Get-Service $srvName
+        Write-Output "$($srvName) is now $($srvStat.status)"
+        WriteLog "$($srvName) is now $($srvStat.status)"
+
+        Start-Sleep -s 10
+        WriteLog "Service started. Now Check logs into UI"
+}
